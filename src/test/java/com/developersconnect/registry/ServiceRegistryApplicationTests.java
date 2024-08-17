8befd0c6cca0749a63ee3c7a4162c1b39e614495
Anusruta Dutta <Anusruta.Dutta@wisetechglobal.com>
2024-08-17 23:24:20 +0530

    Initial commit

---
+++ src/test/java/com/developersconnect/registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developersconnect.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
